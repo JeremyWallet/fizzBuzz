dea5ccb66e94790d511bd63c96340699d37935cc
JeremyWallet <80753696+JeremyWallet@users.noreply.github.com>
2022-11-17 14:55:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 // The loop will stop at 200 and display in console.log the numbers when fizz, buzz and fizzBuzz are not displayed.
 function fizzBuzz(n) {
     for (var i = 1; i < n; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            // Or (i % 15 ==0) but I found this solution less visually understandable
+        if (i % 3 === 0 && i % 5 === 0) {
+            // Or (i % 15 === 0) but I found this solution less visually understandable
             console.log("FizzBuzz");
-        } else if (i % 3 == 0) {
+        } else if (i % 3 === 0) {
             console.log("Fizz");
-        } else if (i % 5 == 0) {
+        } else if (i % 5 === 0) {
             console.log("Buzz");
         } else {
             console.log(i);
